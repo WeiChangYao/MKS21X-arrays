0185d7e0aebb910480faaa2a70f415492ca083f1
weichanhyao <wyao1@stuy.edu>
2018-09-27 21:16:02 -0400

    first part

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,2 +1,12 @@
-class ArrayDemo {
-    public static void main(String[] args) {
+public class ArrayDemo {
+	private static void printArray(int[] ary) {
+		for (int i = 0; i < ary.length; i++) {
+			System.out.print(ary[i] + " ");
+      			}
+   		}
+   	public static void main(String[] args) {
+      		int[] test0a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+      		printArray(test0a);
+   	}
+}
+		
